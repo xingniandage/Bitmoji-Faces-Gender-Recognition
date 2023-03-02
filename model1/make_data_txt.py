62a45f2525c8989e8d9939a58175dbe0f3a5ba4d
xingniandage <96239333+xingniandage@users.noreply.github.com>
2023-03-02 10:27:38 +0800

    Update make_data_txt.py

--- model1/make_data_txt.py
+++ model1/make_data_txt.py
@@ -2,10 +2,10 @@ import os
 import pandas as pd
 
 def make_train_test():
-    point=0.8
+    point=0.8 #训练集和测试集比重
 
-    filename='data/BitmojiDataset/trainimages/'
-    data = pd.read_csv(r'D:\Word  文档\大三上\深度学习\mycode\data\train.csv',sep=',',header='infer',usecols=[0,1])
+    filename='data/BitmojiDataset/trainimages/'#替换成自己存储训练集的位置
+    data = pd.read_csv(r'D:\Word  文档\大三上\深度学习\mycode\data\train.csv',sep=',',header='infer',usecols=[0,1]) #替换成自己存储CSV的位置
     array=data.values[0::,0::]
     print(array.shape)
     for i in range(array.shape[0]):
@@ -24,8 +24,8 @@ def make_train_test():
                     name=filename+array[i][0]+" "+str(array[i][1])+'\n'
                 f.write(name)
 
-def make_detection():
-    filename = 'data/BitmojiDataset/testimages/'
+def make_detection():#制作检验集
+    filename = 'data/BitmojiDataset/testimages/'#替换成自己存储检验集的位置 1084张图片那里
     data = pd.read_csv(r'D:\Word  文档\大三上\深度学习\mycode\data\sample_submission.csv', sep=',', header='infer', usecols=[0, 1])
     array = data.values[0::, 0::]
     print(array.shape)
